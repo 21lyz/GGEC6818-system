e672586c553be985e08e6b999057beb66d8d192a
21lyz <1806913401@qq.com>
2024-09-20 18:39:42 +0800

    First

---
+++ project/cmake-build-debug/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "G:/SharedFile/Project/bmp.cpp" "CMakeFiles/Project.dir/bmp.cpp.obj" "gcc" "CMakeFiles/Project.dir/bmp.cpp.obj.d"
+  "G:/SharedFile/Project/common.cpp" "CMakeFiles/Project.dir/common.cpp.obj" "gcc" "CMakeFiles/Project.dir/common.cpp.obj.d"
+  "G:/SharedFile/Project/lcd.cpp" "CMakeFiles/Project.dir/lcd.cpp.obj" "gcc" "CMakeFiles/Project.dir/lcd.cpp.obj.d"
+  "G:/SharedFile/Project/main.cpp" "CMakeFiles/Project.dir/main.cpp.obj" "gcc" "CMakeFiles/Project.dir/main.cpp.obj.d"
+  "G:/SharedFile/Project/music.cpp" "CMakeFiles/Project.dir/music.cpp.obj" "gcc" "CMakeFiles/Project.dir/music.cpp.obj.d"
+  "G:/SharedFile/Project/photo.cpp" "CMakeFiles/Project.dir/photo.cpp.obj" "gcc" "CMakeFiles/Project.dir/photo.cpp.obj.d"
+  "G:/SharedFile/Project/touch.cpp" "CMakeFiles/Project.dir/touch.cpp.obj" "gcc" "CMakeFiles/Project.dir/touch.cpp.obj.d"
+  "G:/SharedFile/Project/v4l2.cpp" "CMakeFiles/Project.dir/v4l2.cpp.obj" "gcc" "CMakeFiles/Project.dir/v4l2.cpp.obj.d"
+  "G:/SharedFile/Project/video.cpp" "CMakeFiles/Project.dir/video.cpp.obj" "gcc" "CMakeFiles/Project.dir/video.cpp.obj.d"
+  "G:/SharedFile/Project/voice_control.cpp" "CMakeFiles/Project.dir/voice_control.cpp.obj" "gcc" "CMakeFiles/Project.dir/voice_control.cpp.obj.d"
+  "G:/SharedFile/Project/word.cpp" "CMakeFiles/Project.dir/word.cpp.obj" "gcc" "CMakeFiles/Project.dir/word.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/cmake-build-debug/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project.dir/bmp.cpp.obj"
+  "CMakeFiles/Project.dir/bmp.cpp.obj.d"
+  "CMakeFiles/Project.dir/common.cpp.obj"
+  "CMakeFiles/Project.dir/common.cpp.obj.d"
+  "CMakeFiles/Project.dir/lcd.cpp.obj"
+  "CMakeFiles/Project.dir/lcd.cpp.obj.d"
+  "CMakeFiles/Project.dir/main.cpp.obj"
+  "CMakeFiles/Project.dir/main.cpp.obj.d"
+  "CMakeFiles/Project.dir/music.cpp.obj"
+  "CMakeFiles/Project.dir/music.cpp.obj.d"
+  "CMakeFiles/Project.dir/photo.cpp.obj"
+  "CMakeFiles/Project.dir/photo.cpp.obj.d"
+  "CMakeFiles/Project.dir/touch.cpp.obj"
+  "CMakeFiles/Project.dir/touch.cpp.obj.d"
+  "CMakeFiles/Project.dir/v4l2.cpp.obj"
+  "CMakeFiles/Project.dir/v4l2.cpp.obj.d"
+  "CMakeFiles/Project.dir/video.cpp.obj"
+  "CMakeFiles/Project.dir/video.cpp.obj.d"
+  "CMakeFiles/Project.dir/voice_control.cpp.obj"
+  "CMakeFiles/Project.dir/voice_control.cpp.obj.d"
+  "CMakeFiles/Project.dir/word.cpp.obj"
+  "CMakeFiles/Project.dir/word.cpp.obj.d"
+  "Project.exe"
+  "Project.exe.manifest"
+  "Project.pdb"
+  "libProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
